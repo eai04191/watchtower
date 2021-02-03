3c6fccc20efd32a2d715e41dbb24d6013b59cc36
Irie Aoi <eai@mizle.net>
2021-02-03 22:17:43 +0900

    htmlをパースして外部リンクのみ通知するようにした
    close #2
    あとリブログ無視するようにした
    close #3

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { post } from "./webhook";
 import { Masto } from "masto";
+import { parse } from "node-html-parser";
 
 import * as dotenv from "dotenv";
 dotenv.config();
 (async () => {
+    const debug = Boolean(process.env.debug) || false;
+
     const masto = await Masto.login({
         uri: process.env.MASTODON_URI || "",
         accessToken: process.env.MASTODON_ACCESS_TOKEN,
@@ -16,25 +19,29 @@ dotenv.config();
     console.log("Watching Timeline...");
     const stream = await masto.streamUser();
 
-    stream.on("update", (status) => {
+    stream.on("update", async (status) => {
+        debug && console.log(status);
+
         const regex = /えあい/;
         const content = status.content;
 
-        if (content.match(regex) || status.spoilerText.match(regex)) {
-            if (status.account.acct === "Eai") return;
+        // 自身による言及は無視
+        if (status.account.acct === bot.acct) return;
+        //  reblogの場合は無視
+        if (status.reblog) return;
 
+        if (content.match(regex) || status.spoilerText.match(regex) || debug) {
             console.log(`${status.content} by ${status.account.acct}`);
-            post(status.uri);
 
-            // contentにURLが含まれているか
-            const urlRegex = /href="(http.+?)"/gm;
-            const matched = content.match(urlRegex);
-            if (matched) {
-                const urls = Array.from(
-                    content.matchAll(/href="(http.+?)"/g)
-                ).map((u) => Array.isArray(u) && u[1]);
-                post(urls.join("\n"));
-            }
+            // 投稿自体ののurlがあればurlを投稿。なければuriを投稿
+            status.url
+                ? await post("💬 " + status.url)
+                : await post("💬 " + status.uri);
+
+            // contentに外部リンクが含まれていれば投稿
+            const content = parse(status.content);
+            const urls = content.querySelectorAll("a[target='_blank']");
+            urls.forEach((url) => post("🔗 " + url.attributes.href));
         }
     });
 })();
